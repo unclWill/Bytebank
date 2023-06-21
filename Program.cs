9288c7bffa59f88b5eac0df1bcc10b30616cd4ae
William Silva <williamsilvajdf@gmail.com>
2023-06-21 15:18:58 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Bytebank
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Pressione qualquer tecla para finalizar...");
+            Console.ReadKey();
+        }
+
+        public static void ExecutarComandos()
+        {
+        }
+
+        public static void MenuInicial()
+        {
+            Console.WriteLine("Boas vindas ao Terminal do Bytebank!");
+
+        }
+
+    }
+
+    public static class CorDoTexto
+    {
+        public static void Amarelo() { Console.ForegroundColor = ConsoleColor.DarkYellow; }
+        public static void Azul() { Console.ForegroundColor = ConsoleColor.DarkBlue; }
+        public static void Cinza() { Console.ForegroundColor = ConsoleColor.DarkGray; }
+        public static void Verde() { Console.ForegroundColor = ConsoleColor.DarkGreen; }
+        public static void Vermelho() { Console.ForegroundColor = ConsoleColor.DarkRed; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+## Bytebank
+
+- Projeto Bytebank, do curso C#: Orientação a Objetos, da Alura.
+
+* Objetivos do curso:
+  Desenvolver uma solução para o Bytebank, um banco digital fictício, que precisa ter um melhor controle sobre as contas correntes de seus clientes.
