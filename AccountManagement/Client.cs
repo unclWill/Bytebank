4a71d5e6f6c4217c122dc97b65ea455d2577bd84
William Silva <william.silva@viannasempre.com.br>
2023-07-24 17:13:58 -0300

    Alterações na classe Client

--- AccountManagement/Client.cs
+++ AccountManagement/Client.cs
@@ -1,7 +1,7 @@
 /* Classe  : Client
  * Objetivo: Concentra as lógicas de criação de clientes.
  * Autor   : unclWill (williamsilvajdf@gmail.com)
- * Data    : 27/06/2023 (Criação) | Modificação: 27/06/2023
+ * Data    : 27/06/2023 (Criação) | Modificação: 24/07/2023
  */
 
 using System;
@@ -10,6 +10,8 @@ namespace Bytebank.AccountManagement
 {
     public class Client
     {
+        public string? AccountHolder { get; set; }
+
 
     }
 }
